50906dd319725336173aa6cbd6fd510a2295b19a
Elnura D <elnurab@gmail.com>
2021-02-19 15:34:53 +0800

    adding courseinfo files

---
+++ part1/courseinfo/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+
+}
+
+const Part = (props) =>{
+  console.log(props)
+  return (
+    <div>
+      {/* <p>{props.part} {props.exercises}</p> */}
+      {props.parts.map((value, index)=><p key={index}>{value.name} {value.exercises}</p>)}
+    </div>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part parts={props.parts} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+    <div>
+       <p>Number of exercises {props.parts.reduce((sum, item)=> sum + item.exercises, 0)}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
